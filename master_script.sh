994231893283e52654004233690d3922340874ac
Bethany Schefter <bschefte@uwo.ca>
2020-09-22 20:43:05 -0400

    added master_script

---
+++ master_script.sh
@@ -0,0 +1,16 @@
+cd
+cd Desktop/assignment_2/sample
+mkdir "m31"
+mkdir "m81"
+mkdir "ngc0337"
+mkdir "unknown_project_files"
+cd  m31
+mkdir "external_scripts_or_compiled_programs"
+mkdir "data"
+mkdir "doc_and_project_info"
+mkdir "everything_graphic_results"
+mkdir "src_main_programs"
+cd  data
+mkdir "extracted_data"
+mkdir "raw_data"
+mkdir "results"
