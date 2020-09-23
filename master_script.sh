e86466d8e6f5aa0b4f7fdce9ed6514d72b0dfee6
Bethany Schefter <bschefte@uwo.ca>
2020-09-22 21:36:23 -0400

    moved all non-m31 files to the correct project folder

--- master_script.sh
+++ master_script.sh
@@ -15,8 +15,17 @@ cd  data
 mkdir "extracted_data"
 mkdir "raw_data"
 mkdir "results"
-#Sort into project folders
+#Sort non-m31 files into project folders
 cd
 cd Desktop/assignment_2/sample
 mv M81* m81/
 mv *ngc0337* ngc0337/ #don't worry about directory warning. It will only move the files
+mv broadband_phot unknown_project_files #start of unknown files
+mv els_sfr*.reg unknown_project_files
+mv ia_session.log unknown_project_files
+mv iras08572_smith2010.dat unknown_project_files
+mv IRS-egal-bgd_Kaneda_June20_2015.txt unknown_project_files
+mv nucFLUX unknown_project_files
+mv nuclear_spectra.ipynb unknown_project_files
+mv nucUNC unknown_project_files
+mv pahfit_sile.pro unknown_project_files #end of unknown files
